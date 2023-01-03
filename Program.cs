5c0edb47f4be8e26dce161207fdaff281b87236f
Szymon123I <113946225+Szymon123I@users.noreply.github.com>
2023-01-03 14:34:50 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace for_each
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        int suma = 0;
+        int[] dane = new int[100];
+        for (int i = 1; i < dane.Length; i++)
+            {
+                dane[i] = i;
+            }
+        foreach (int x in dane)
+        {
+            suma += x;
+        }
+        Console.WriteLine(suma);
+    }
+}
+}
